1efe9f3883aca9e942e3346c201280e6d74b33a8
PStewart1 <landofalcon007@gmail.com>
2022-09-07 13:44:59 -0500

    Wrote a function that replaces an element in a list at a specific position without modifying the original list

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list is not None: 
+    if my_list is not None:
         for i in range(len(my_list) - 1, -1, -1):
             print("{:d}".format(my_list[i]))
