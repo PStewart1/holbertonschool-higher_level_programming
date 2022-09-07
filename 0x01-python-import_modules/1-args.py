81148aa7d12205cfadc61df055c384708647b6da
PStewart1 <landofalcon007@gmail.com>
2022-09-07 12:35:48 -0500

    added colons in argument statement

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -4,10 +4,12 @@ import sys
 
 if __name__ == "__main__":
     n = len(sys.argv) - 1
-    if n != 1:
-        print("{} arguments.".format(n))
+    if n == 0:
+        print("0 arguments.")
     elif n == 1:
-        print("{} argument.".format(n))
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
     if n > 0:
         for i in range(1, n + 1):
             print("{}: {}".format(i, sys.argv[i]))
