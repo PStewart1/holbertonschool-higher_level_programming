bca0029a917d27c963e14cef9bae7218df12bb7f
PStewart1 <landofalcon007@gmail.com>
2022-09-07 10:48:11 -0500

    Wrote a class Square that defines a square.

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """A class to represent a Square."""
+    def __init__(self, size=0):
+        """Constructs all the necessary attributes for the square object.
+
+        Args:
+            size (int): size of square.
+
+        """
+        self.__size = size
