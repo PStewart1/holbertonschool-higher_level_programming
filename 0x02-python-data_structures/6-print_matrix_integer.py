fe049e8a28c162ec59ad7a2fffdae317a35419f8
PStewart1 <landofalcon007@gmail.com>
2022-09-08 15:25:44 -0500

    added :d

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                print("{}".format(matrix[i][j]), end='')
+                print("{:d}".format(matrix[i][j]), end='')
                 if j < (len(matrix[i]) - 1):
                     print(" ", end='')
             print()
