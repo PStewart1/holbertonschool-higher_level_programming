a09c6d57abfdb26c181c1442bb04ed1bf844b629
PStewart1 <landofalcon007@gmail.com>
2022-10-05 16:11:20 -0500

    removed dunder from attrs in init

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -29,8 +29,8 @@ class Rectangle:
             height : int
                 height of rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
