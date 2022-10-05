261cf0777f21a0847e850a393c4422ad5c3072f6
PStewart1 <landofalcon007@gmail.com>
2022-10-05 15:07:05 -0500

    Wrote a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0

---
+++ 0x00-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+--  lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+--  The database name will be passed as an argument to the mysql command
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score;
