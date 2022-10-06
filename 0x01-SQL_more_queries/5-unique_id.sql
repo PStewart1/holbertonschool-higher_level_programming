fad5d03e2d479ac4ae9f3c36fdb674df4bc750d4
PStewart1 <landofalcon007@gmail.com>
2022-10-06 10:45:48 -0500

    Wrote a script that creates the table unique_id

---
+++ 0x01-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,3 @@
+-- creates the table unique_id
+-- The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
