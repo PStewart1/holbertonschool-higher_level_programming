1bed289eea76ea6a95b6b513f6055b08256ead48
PStewart1 <landofalcon007@gmail.com>
2022-10-06 10:38:17 -0500

    Wrote a script that creates the table id_not_null

---
+++ 0x01-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- creates the table id_not_null
+-- The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
