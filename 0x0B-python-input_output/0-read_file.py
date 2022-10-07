6de428b668887464dd1e42258f3541a51d39647d
PStewart1 <landofalcon007@gmail.com>
2022-10-07 10:30:26 -0500

    removed newline in print statement

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """ reads a text file (UTF8) and prints it to stdout """
     with open(filename, 'r') as f:
         txt = f.read()
-        print(txt)
+        print(txt, end='')
         f.close()
