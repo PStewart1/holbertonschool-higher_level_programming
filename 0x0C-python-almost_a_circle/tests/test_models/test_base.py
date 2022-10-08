c0220ef025b49ba954a096d67e18ccafd143c4dc
PStewart1 <landofalcon007@gmail.com>
2022-10-08 17:26:47 -0500

    added tests for save_to_file()

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
 """ Unittests for Base class """
 import unittest
+from models.square import Square
 from models.base import Base
+import os.path
 
 
 class TestBase(unittest.TestCase):
     """ test class for Base class """
 
-    def test_diff_id(self):
+    def test_sequence(self):
         b1 = Base()
         b2 = Base()
-        self.assertNotEqual(b1.id, b2.id)
+        self.assertEqual(b2.id, 5)
 
-    def test_give_id(self):
-        b1 = Base(12)
-        self.assertEqual(b1.id, 12)
-
-    def test_sequence(self):
+    def test_diff_id(self):
         b3 = Base()
         b4 = Base()
-        b5 = Base()
-        self.assertEqual(b5.id, 5)
+        self.assertNotEqual(b3.id, b4.id)
+
+    def test_give_id(self):
+        b5 = Base(12)
+        self.assertEqual(b5.id, 12)
 
     def test_json(self):
         b6 = Base(12)
@@ -29,3 +30,15 @@ class TestBase(unittest.TestCase):
     def test_json2(self):
         b6 = Base(13)
         self.assertEqual(Base.to_json_string(None), '[]')
+
+    def test_save(self):
+        s1 = Square(1)
+        Square.save_to_file([s1])
+        self.assertTrue(os.path.isfile('Square.json'))
+
+    def test_save2(self):
+        s2 = Square(id=92, size=2, x=0, y=1)
+        Square.save_to_file([s2])
+        with open("Square.json", "r") as file:
+            self.assertEqual(file.read(),
+                             '[{"id": 92, "size": 2, "x": 0, "y": 1}]')
