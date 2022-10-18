f63853eec4a18760c8a790945144e7368638b357
PStewart1 <landofalcon007@gmail.com>
2022-10-18 12:26:05 -0500

    Wrote a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    try:
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print("MySQL Error [%d]: %s" % (e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Error: %s" % str(e))
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
