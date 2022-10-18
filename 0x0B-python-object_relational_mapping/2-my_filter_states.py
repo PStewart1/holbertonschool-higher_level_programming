a342917b73274b3c31e3b08cd9d999d33a86164d
PStewart1 <landofalcon007@gmail.com>
2022-10-18 13:25:23 -0500

    made unsafe

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
     try:
-        cur.execute(
-            'SELECT * FROM states WHERE name = "{}" ORDER BY id ASC'
-            .format(argv[4]))
+        cur.execute(query)
         rows = cur.fetchall()
     except MySQLdb.Error as e:
         try:
