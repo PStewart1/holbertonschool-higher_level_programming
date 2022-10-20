e3d0292895ce02ccaf4c3125947e9ebd3ab159a1
PStewart1 <landofalcon007@gmail.com>
2022-10-20 13:16:10 -0500

    added the empty check

--- 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,5 +17,8 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
     session.close()
