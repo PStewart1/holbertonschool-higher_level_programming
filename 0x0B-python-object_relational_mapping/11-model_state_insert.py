d09ad13f566ecfa064422fca28b3ad8a5b851194
PStewart1 <landofalcon007@gmail.com>
2022-10-20 14:47:32 -0500

    made top docstring 2 lines

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" adds the State object “Louisiana” to the database hbtn_0e_6_usa """
+""" adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine, insert
