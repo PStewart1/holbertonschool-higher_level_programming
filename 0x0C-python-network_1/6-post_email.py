39c979f9fea448681837b5f4b1f3292c83848dc4
PStewart1 <landofalcon007@gmail.com>
2022-11-01 15:32:28 -0500

    A script that POSTs a given email and displays response

---
+++ 0x0C-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    response = requests.post(url, email)
+    html = response.text
+    print("{}".format(html))
