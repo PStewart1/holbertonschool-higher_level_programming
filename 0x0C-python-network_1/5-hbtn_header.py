3f760d34d0ef0e44cddfe6decc5a9af2ba36b37e
PStewart1 <landofalcon007@gmail.com>
2022-11-01 15:18:36 -0500

    added .get for dict retrievel

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    html = response.headers['X-Request-Id']
+    html = response.headers.get('X-Request-Id')
     print("{}".format(html))
