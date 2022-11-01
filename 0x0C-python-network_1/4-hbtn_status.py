9dde9a95fddd7597f90b4c99da9c9be41a4b972e
PStewart1 <landofalcon007@gmail.com>
2022-11-01 14:52:51 -0500

    replaced urllib with requests

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """ fetches https://intranet.hbtn.io/status
 """
-from urllib import request
-# import sys
+import requests
 
 
 if __name__ == "__main__":
-    req = request.Request("https://intranet.hbtn.io/status")
-    with request.urlopen(req) as response:
-        html = response.read()
+    with requests.get("https://intranet.hbtn.io/status") as response:
+        html = response.text
         print("Body response:")
-        print("\t- type: {}".format(type(html.decode())))
-        print("\t- content: {}".format(str(html, "utf-8")))
-
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
