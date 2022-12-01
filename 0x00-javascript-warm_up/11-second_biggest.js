2e447aabc90a4d1d3a618a49f41221a3bff6c1dd
PStewart1 <4920@holbertonstudents.com>
2022-12-01 15:32:52 -0600

    searches the second biggest integer in the list of arguments

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+let big;
+let biggest = args[0];
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  for (let i = 0; i <= args.length; i++) {
+    if (args[i] > biggest) {
+      big = biggest;
+      biggest = args[i];
+    }
+  }
+  console.log(big);
+}
