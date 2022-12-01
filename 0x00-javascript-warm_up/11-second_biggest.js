afb6b7af63ac1f37192ab8bd1438e9f31e209795
PStewart1 <4920@holbertonstudents.com>
2022-12-01 15:34:51 -0600

    changed 0 to string

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 let big;
 let biggest = args[0];
 if (args.length <= 1) {
-  console.log(0);
+  console.log('0');
 } else {
   for (let i = 0; i <= args.length; i++) {
     if (args[i] > biggest) {
