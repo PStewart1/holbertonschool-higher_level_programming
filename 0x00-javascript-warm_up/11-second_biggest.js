45d24f1cdef7e9c6f7beafd7c0b73fd4973b8ead
PStewart1 <4920@holbertonstudents.com>
2022-12-01 15:51:33 -0600

    added else if to handle biggest value coming first, and initialized to -inf

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-let big;
-let biggest = args[0];
+let big = -Infinity;
+let biggest = -Infinity;
 if (args.length <= 1) {
-  console.log('0');
+  console.log(0);
 } else {
   for (let i = 0; i <= args.length; i++) {
     if (args[i] > biggest) {
       big = biggest;
       biggest = args[i];
+    } else if (args[i] < biggest && args[i] > big) {
+      big = args[i]
     }
   }
   console.log(big);
