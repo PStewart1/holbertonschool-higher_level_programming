b961728edc3d6be53097144f77ff5a62db65d4ce
PStewart1 <4920@holbertonstudents.com>
2022-12-02 13:58:14 -0600

    class Rectangle w/ width, height, checks, and a print method

---
+++ 0x01-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (!isNaN(w) && !isNaN(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let x;
+    for (let i = 1; i <= this.height; i++) {
+      x = '';
+      for (let j = 1; j <= this.width; j++) {
+        x = x + 'X';
+      }
+      console.log(x);
+    }
+  }
+};
