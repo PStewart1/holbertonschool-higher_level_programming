f6eec82a7de2bef1236250e732f461bdebef227d
PStewart1 <4920@holbertonstudents.com>
2022-12-02 15:42:44 -0600

    added function into sort() to handle double-digits

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2).sort().reverse();
+const args = process.argv.slice(2).map(num => parseInt(num));
 if (args.length <= 1) {
   console.log(0);
 } else {
+  args.sort(function (a, b) {
+    return a - b;
+  }).reverse();
   for (let i = 1; i < args.length; i++) {
     if (args[i] < args[i - 1]) {
       console.log(args[i]);
