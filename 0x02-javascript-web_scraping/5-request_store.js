fdce8f24630135bdb74d369ce2fb6321dace11a1
PStewart1 <4920@holbertonstudents.com>
2022-12-06 15:52:57 -0600

    gets the contents of a webpage and stores it in a file.

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+const options = { json: true };
+request(url, options, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  }
+  fs.writeFile(file, body, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
