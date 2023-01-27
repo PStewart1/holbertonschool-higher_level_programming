4731cdd9a464092f82cf4daf2f41bcf55c64bb54
PStewart1 <4920@holbertonstudents.com>
2023-01-27 15:40:43 -0600

    Wrote a class with one method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains the class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class contains method area(self)"""
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
