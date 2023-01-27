ee0b28c931066e15e1a66756cde9b5d068abb39b
PStewart1 <4920@holbertonstudents.com>
2023-01-27 16:25:21 -0600

    changed [Rectangle] to [Square]

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains the class Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class inherits from Rectangle, contains property size
+    and method area"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
