0aad161cdea0ca21cd2216ca00fd6e366687f0ef
PStewart1 <4920@holbertonstudents.com>
2023-01-28 17:49:35 -0600

    added update(), which assigns an argument to each attribute

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,21 @@ class Square(Rectangle):
             raise ValueError("width must be > 0")
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigns an argument to each attribute"""
+
+        if not args:
+            for key, value in kwargs.items():
+                if key != 'id' and key != 'size':
+                    key = '_Rectangle__' + key
+                    self.__dict__.update({key: value})
+                elif key == 'size':
+                    self.__dict__.update({'_Rectangle__width': value})
+                else:
+                    self.__dict__.update({key: value})
+        else:
+            attrs = ['id', '_Rectangle__width',
+                     '_Rectangle__x', '_Rectangle__y']
+            for i in range(len(args)):
+                self.__dict__.update({attrs[i]: args[i]})
