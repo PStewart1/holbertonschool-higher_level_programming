b195b28c4b0938643eadadb3dcb84efceac5da2f
PStewart1 <4920@holbertonstudents.com>
2023-01-28 15:18:55 -0600

    Wrote a class Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains the class Student"""
+
+
+class Student():
+    """class defines a student;
+    instantiates with first_name, last_name and age;
+    includes method to_json """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retrieves a dictionary representation of a Student instance"""
+        return self.__dict__
