7162357f3030f3ff74d846cc11f34cbd9cc80cbf
PStewart1 <4920@holbertonstudents.com>
2023-01-28 18:03:12 -0600

    added to_json_string() which returns a JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ 0-main """
+import json
 
 
 class Base():
@@ -13,3 +14,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns the JSON string representation of list_dictionaries"""
+
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
